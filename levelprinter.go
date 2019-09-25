06d9cee538dbf927840555cf736ac351c90d24da
Erik Unger <erik@erikunger.com>
2019-09-25 14:32:37 +0200

    add more compatibility to LevelPrinter

--- levelprinter.go
+++ levelprinter.go
@@ -1,16 +1,34 @@
 package golog
 
-// type Printer interface {
-// 	Printf(format string, v ...interface{})
-// }
+import (
+	"fmt"
+	"log"
+)
 
-// LevelPrinter is a compatibility shim to be able to use
-// a Logger with a Level for third party packages
-// that need an an interface implementation with
-// a Printf(string, ...interface{}) method.
+// LevelPrinter is a compatibility shim that enables
+// using a Logger with a Level for third party packages
+// that either need an an interface implementation with
+// a Printf method, an io.Writer, or a standard log.Logger.
 type LevelPrinter struct {
 	logger *Logger
 	level  Level
+}
+
+func (p *LevelPrinter) Write(data []byte) (int, error) {
+	p.Msg(string(data))
+	return len(data), nil
+}
+
+func (p *LevelPrinter) Msg(msg string) {
+	p.logger.NewMessage(p.level, msg).Log()
+}
+
+func (p *LevelPrinter) Print(v ...interface{}) {
+	p.Msg(fmt.Sprint(v...))
+}
+
+func (p *LevelPrinter) Println(v ...interface{}) {
+	p.Msg(fmt.Sprintln(v...))
 }
 
 func (p *LevelPrinter) Printf(format string, v ...interface{}) {
@@ -21,4 +39,8 @@ func (p *LevelPrinter) Func() func(format string, v ...interface{}) {
 	return func(format string, v ...interface{}) {
 		p.Printf(format, v...)
 	}
+}
+
+func (p *LevelPrinter) StdLogger(prefix string, flag int) *log.Logger {
+	return log.New(p, prefix, flag)
 }
